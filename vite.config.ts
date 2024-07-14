940891741899aa61704866dd4541fc429973f835
ddonnelly19 <bluesteelkc@gmail.com>
2024-07-14 16:51:02 +0000

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import * as monacoEditorPlugin from "vite-plugin-monaco-editor";
+import monacoEditorPlugin from "vite-plugin-monaco-editor";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    (monacoEditorPlugin as any).default.default({}),
-  ],
+	plugins: [
+		react(),
+		(monacoEditorPlugin).default({
+
+		}),
+	],
 });
